3c550d7e01f0a585d515169957268aac4a0a8823
Muhammed Shifan P <muhammedshifanpazheri@gmail.com>
2022-12-29 17:14:43 +0530

    added product details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import data from "./data";
 
 function App() {
   return (
@@ -7,7 +8,25 @@ function App() {
       <header>
         <a href="/">ozge_abayas</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>list products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div key={product.slug} className="product">
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name}></img>
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>{product.price}</p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
