00dbcc1e7b766a88a507435cebc4dca4b69be7f2
Muhammed Shifan P <muhammedshifanpazheri@gmail.com>
2022-12-29 15:35:16 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">ozge_abayas</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
