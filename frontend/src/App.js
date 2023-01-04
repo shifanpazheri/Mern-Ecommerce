e2622161302e858e7f08ba2bb191d4fc45b6ef75
Muhammed Shifan P <muhammedshifanpazheri@gmail.com>
2023-01-04 17:51:01 +0530

    adding bootstrap UI framework

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,34 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">ozge_abayas</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Özge Abayas</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductPage />} />
-            <Route path="/" element={<HomePage />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductPage />} />
+              <Route path="/" element={<HomePage />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All right reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
