da77cf1c7296d20721e3e557377bb69873275409
Muhammed Shifan P <muhammedshifanpazheri@gmail.com>
2023-01-11 11:01:21 +0530

    Create Cart using React Context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,17 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import { LinkContainer } from "react-router-bootstrap";
+import { useContext } from "react";
+import { Store } from "./Store";
+import Badge from "react-bootstrap/esm/Badge";
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
+
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
@@ -15,6 +22,16 @@ function App() {
               <LinkContainer to="/">
                 <Navbar.Brand>Özge Abayas</Navbar.Brand>
               </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.length}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
             </Container>
           </Navbar>
         </header>
