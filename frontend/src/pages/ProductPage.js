f1051cf2228469ab5283c671453793a159394fb4
Muhammed Shifan P <muhammedshifanpazheri@gmail.com>
2023-01-11 15:33:04 +0530

    Complete ADD TO CART

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -52,10 +52,20 @@ function ProductPage() {
 
   const { state, dispatch: ctxDispatch } = useContext(Store);
 
-  const addToCartHandler = () => {
+  const addToCartHandler = async () => {
+    const { cart } = state;
+    const existItem = cart.cartItems.find((item) => {
+      return item._id === product._id;
+    });
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get("/api/products/" + product._id);
+    if (data.countInStock < quantity) {
+      window.alert("Sorry. Product is out of stock");
+      return;
+    }
     ctxDispatch({
       type: "CART_ADD_ITEM",
-      payload: { ...product, quantity: 1 },
+      payload: { ...product, quantity },
     });
   };
 
