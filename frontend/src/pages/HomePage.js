94d2d1dd802bd293585c6a8100a3546bab9c5d74
Muhammed Shifan P <muhammedshifanpazheri@gmail.com>
2023-03-06 01:03:47 +0530

    align images in center

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -56,7 +56,13 @@ function HomePage() {
         ) : (
           <Row>
             {products.map((product) => (
-              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+              <Col
+                key={product.slug}
+                sm={6}
+                md={4}
+                lg={3}
+                className="mb-3 text-center"
+              >
                 <Product product={product} />
               </Col>
             ))}
