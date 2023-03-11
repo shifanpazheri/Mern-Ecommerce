728103546b22e5ad7221adffbe88ad2ed9cf251e
Muhammed Shifan P <muhammedshifanpazheri@gmail.com>
2023-03-11 15:28:55 +0530

    removing warnings

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useState } from "react";
+import React, { useEffect, useReducer } from "react";
 import axios from "axios";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
