899276d953176136e9d93606041c590728694238
Muhammed Shifan P <muhammedshifanpazheri@gmail.com>
2023-07-16 15:38:39 +0530

    footer updated

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,6 +4,7 @@ export default function Footer() {
   return (
     <div className="text-center bg-dark text-light">
       <p>All right reserved by Ozge Abayes</p>
+      <p> Contact Us at https://www.instagram.com/ozge_abayas/</p>
     </div>
   );
 }
